8c482634d2872449fcd62597f6ca4fd9d9b75e6f
corcel2000 <corcelmreeves@gmail.com>
2025-04-12 08:42:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,9 @@ document.addEventListener("DOMContentLoaded", function () {
         if (password.length < 8) {
             alert("Password must be at least 8 characters long.");
         }
-
+	    
+        localStorage.setItem("registrationData", JSON.stringify(users));
+        alert("Registration successful!");
         // TRN format
         const trnPattern = /^\d{3}-\d{3}-\d{3}$/;
         if (!trnPattern.test(trn)) {
