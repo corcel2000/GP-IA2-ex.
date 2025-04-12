9047a54e7c0e277113937cefc17d917aac3b2bfe
corcel2000 <corcelmreeves@gmail.com>
2025-04-11 23:01:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -87,11 +87,13 @@ document.getElementById("registerForm").addEventListener("submit", function (eve
             }
 	
             // Save to localStorage
+	document.getElementById("register-btn").addEventListener("click", function () {
 	    alert("Registration successful!");
 	    window.location.href = "login.html";
             users.push(newUser);
             localStorage.setItem("RegistrationData", JSON.stringify(users));
             document.getElementById("registerForm").reset();
+	});	
 
 	 // Create new user object
             const newUser = {
